66753c6f8b3c470f7aa9ba197c20e10e97ba4fe8
Crystal Young <crystal.young@codeimmersives.com>
2020-07-09 14:08:42 -0400

    add

---
+++ main.js
@@ -0,0 +1,27 @@
+const emagiSearch = require('./emagi-search.js');
+const emagify = require('./emagify.js');
+const randomElement = require('./emagi-random.js');
+
+
+function randonElement(random) {
+    return random()
+}
+
+function getInput(i) {
+    return ProcessingInstruction.argv[i + 2];
+} if ( getInput <= String.length) {
+return = getInput + '' + '.';
+} else if (emagiSearch === 'undefined') {
+    return.statusMessage('I can/t find that emoji');
+} else {
+    return getInput(i)
+}
+
+function emagiSearch(convert) {
+    return convert.constructor.toString().indexOf
+        ([i]) > -1;
+      }
+// return convert.splice(0, 2)
+console .log(*) 
+    
+}
