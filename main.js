07e6b57e42eae0c54e0bb0152ea926180fa8fcff
Crystal Young <crystal.young@codeimmersives.com>
2020-07-10 10:11:57 -0400

    add

--- main.js
+++ main.js
@@ -2,14 +2,24 @@ const emagiSearch = require('./emagi-search.js');
 const emagify = require('./emagify.js');
 const randomElement = require('./emagi-random.js');
 
-
-
-const search = process.argv[2]
-const searchResult = emagiSearch(search)
-const convert = emagiSearch(search)
 const isIt = emagiSearch(search)
-
+const search = process.argv[2]
+const randomElement = process.argv[2]
+const convert = process.argv[2]
+const word = process.argv[3]
+const quadStr = process.argv[4]
+const searchResult = emagiSearch(search)
 
 console.log(searchResult)
-console.log('Is this the appropiate ' + isIt)
 
+let result = '';
+if (search && quadStr || randomElement && word) {
+    result = `output`;
+} else if (search && word) {
+    result = searchEmojis(word).join('')
+} else if (convert && userText) {
+    result = convertWords(userText).join('')
+} else if (randomElement) {
+    result = randomEmojis(isIt)
+}
+console.log(result)
